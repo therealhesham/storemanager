c1ee66d2c28e94521c2cadaf35567fe23a4f64c8
therealhesham <68483818+therealhesham@users.noreply.github.com>
2023-07-31 17:30:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var XLSX = require('xlsx')
 
 // console.log(obj.data)
 app.get("/", (req,res)=>{
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://nateega.vercel.app/");
     var workbook = XLSX.readFile('./Stage_New_Search.xlsx');
     var sheet_name_list = workbook.SheetNames;
     var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
