1498f9fb01f30eef11795864decdd994af984958
therealhesham <heshammoha231992@gmail.com>
2023-07-31 16:51:43 +0300

    commit on master

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express =require("express")
+
+app = express()
+
+app.use(express.json())
+var xlsx = require('node-xlsx');
+const fs = require("fs")
+
+var XLSX = require('xlsx')
+// var xlsx = require('node-xlsx');
+
+// var obj = xlsx.parse('./Stage_New_search.xlsx'); // parses a file
+
+// console.log(obj.data)
+app.get("/", (req,res)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    var workbook = XLSX.readFile('./Stage_New_search.xlsx');
+    var sheet_name_list = workbook.SheetNames;
+    var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+   
+// console.log("r")
+res.send(xlData)
+
+})
+
+
+
+
+
+app.listen(3000,()=>console.log("hello"))
--- public/javascript.js
+++
@@ -1,7 +0,0 @@
-var $selectstore = $( '#selectstore' ),
-		$contractor = $( '#contractor' ),
-    $options = $select2.find( 'option' );
-    
-$selectstore.on( 'change', function() {
-	$contractor.html( $options.filter( '[value="' + this.value + '"]' ) );
-} ).trigger( 'change' );
